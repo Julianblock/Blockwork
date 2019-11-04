d82dc064a190f17fdf79aed13059b3854590d976
sblatt <sblatt@centricsoftware.com>
2019-11-03 23:16:05 -0800

    Fix logo and add sign up form

--- src/App.vue
+++ src/App.vue
@@ -12,6 +12,18 @@ export default {
   data () {
     return {
       loggedin: false,
+      signUpMode: false,
+    }
+  },
+  methods: {
+    signUp() {
+      this.signUpMode = true;
+    },
+    toLogin() {
+      this.signUpMode = false;
+    },
+    addUser() {
+      
     }
   }
 }
@@ -19,14 +31,31 @@ export default {
 
 <template>
   <div id="app" class="app">
-    <div v-if='!loggedin' >
+    <div v-if='!loggedin && !signUpMode' >
       <div class='login login-background'>
           <div class='login-background-overlay'>
               <div class='login-interface'>
                   <img src='../src/assets/logo.png' />
-                  <input placeholder="Username:" class='login-input' />
-                  <input placeholder="password" class='login-input' type="password" />
+                  <input placeholder="Username" class='login-input' type="text" />
+                  <input placeholder="Password" class='login-input' type="password" />
                   <div class='login-submit' v-on:click="loggedin = true">Submit</div>
+                  <div class='sign-up' v-on:click="signUp()">Sign Up</div>
+              </div>
+          </div>
+      </div>
+    </div>
+    <div v-else-if='!loggedin && signUpMode' >
+      <div class='login login-background'>
+          <div class='login-background-overlay'>
+              <div class='login-interface'>
+                  <img src='../src/assets/logo.png' />
+                  <div v-if="signUpMode" class="sign-up-back-button" v-on:click="toLogin()">Back</div>
+                  <input placeholder="Email" class='login-input' type="text" />
+                  <input placeholder="Phone" class='login-input' type="text" />
+                  <input placeholder="Username" class='login-input' type="text" />
+                  <input placeholder="Password" class='login-input' type="password" />
+                  <div class='login-submit' v-on:click="addUser()">Sign Up</div>
+                  <div v-if="!signUpMode" class='sign-up' v-on:click="signUp()">Sign Up</div>
               </div>
           </div>
       </div>
@@ -43,4 +72,5 @@ export default {
 
 <style lang="scss">
   @import './styles/index.scss';
+
 </style>
